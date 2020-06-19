f876a31028f33c33e4e088f10255eadacb6e497b
Anthony Nahas <anthony.na@hotmail.de>
2020-06-19 09:32:35 +0200

    feat(demo): updated the demo app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputCounterModule} from '@angular-material-extensions/input-counter';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputCounterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
